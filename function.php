933134f00195bfc49743140e3bb854e9de951d5b
bunyodbek9449 <60098383+bunyodbek9449@users.noreply.github.com>
2021-04-03 16:03:51 +0500

    qoshimcha funksiya qoshildi
    qoshimcha funksiya qoshildi

--- function.php
+++ function.php
@@ -6,7 +6,7 @@ class GreatValue {
     public $value3;
     public $value4 = 0;
 
-    public function mainFunction($value1, $value2, $value3){
+    public function engKattasi($value1, $value2, $value3){
          $value4 = $value1;
          if($value4 > $value2){
              if($value4 > $value3){
@@ -20,5 +20,21 @@ class GreatValue {
                  echo "$value3 barcha sonlardan katta";
              }
          } 
+    }
+
+    public function engKichigi($value1, $value2, $value3){
+        $value4 = $value1;
+        if($value4 < $value2){
+            if($value4 < $value3){
+                echo "$value4 barcha sonlardan kichkina";
+            }
+        }else{
+            $value4 = $value2;
+            if($value4 < $value3){
+                echo "$value4 barcha sonlardan kichkina"; 
+            }else{
+                echo "$value3 barcha sonlardan kichkina";
+            }
+        }
     }
 }
