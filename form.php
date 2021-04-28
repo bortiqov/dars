b7954fa4ed6bfbd8602bc4e8f0b9a4f78eb6454a
Muhammadjon  Yuldashev <81702499+yuldashevmuhammad@users.noreply.github.com>
2021-04-28 10:25:34 +0500

    clear clear and end

---
+++ form.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Form</title>
+</head>
+<body>
+
+	<form action="welcome.php" method="post">
+
+		<label for="username">username</label>
+		<input type="text" name="username" id="username"><br>
+
+		<label for="password">password</label>
+		<input type="password" name="password" id="password"><br><br>
+
+		<button type="submit">Login</button>
+	</form>
+
+</body>
+</html>
